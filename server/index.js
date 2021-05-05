b6e2adb483e594649414ff2663b62c7751f7f87e
codadept <info.pratikmajumdar@gmail.com>
2021-05-05 22:02:07 +0530

    added functionality of socket.io

--- server/index.js
+++ server/index.js
@@ -3,5 +3,17 @@ const io = require('socket.io')(8000);
 const users = {}
 
 io.on('connection', socket =>{
-    socket.on()
+    socket.on("new-user-joined",(name)=>{
+        users[socket.id] = name;
+        socket.broadcast.emit("user-joined",name);
+    })
+
+    socket.on("send",message=>{
+        socket.broadcast.emit("receive",{name:users[socket.id],message:message})
+    })
+
+    socket.on("disconnect",message=>{
+        socket.broadcast.emit("left",users[socket.id]);
+        delete users[socket.id];
+    })
 })
