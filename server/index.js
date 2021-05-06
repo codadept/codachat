01c542ddbb733ce462df5d89071f2ef1cb536f2e
codadept <info.pratikmajumdar@gmail.com>
2021-05-06 07:22:11 +0530

    update user list functionality added

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ const io = require('socket.io')(8000);
 const users = {}
 
 io.on('connection', socket =>{
-    socket.on("new-user-joined",(name)=>{
-        users[socket.id] = name;
-        socket.broadcast.emit("user-joined",name);
+    socket.on("new-user-joined",(data)=>{
+        users[socket.id] = data.nameInput;
+        socket.broadcast.emit("user-joined",data);
     })
 
     socket.on("send",message=>{
